fe42b010213c1b9775431343af539dad1f1d6cd9
Ben Wilson <ben@registrationmax.com>
2023-07-22 18:40:01 -0400

    nerdminer fix

--- src/app/pipes/number-suffix.pipe.ts
+++ src/app/pipes/number-suffix.pipe.ts
@@ -10,11 +10,14 @@ export class NumberSuffixPipe implements PipeTransform {
 
     const suffixes = ['', 'k', 'M', 'B', 'T', 'P', 'E'];
 
-    if (value === 0) {
+    if (value == null || value < 0) {
       return '0';
     }
 
-    const power = Math.floor(Math.log10(value) / 3);
+    let power = Math.floor(Math.log10(value) / 3);
+    if (power < 0) {
+      power = 0;
+    }
     const scaledValue = value / Math.pow(1000, power);
     const suffix = suffixes[power];
 

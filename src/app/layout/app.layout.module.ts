22bc9ed26644d95d716634ed49b504e20316a588
Ben Wilson <ben@registrationmax.com>
2023-08-03 16:47:01 -0400

    package optimizations

--- src/app/layout/app.layout.module.ts
+++ src/app/layout/app.layout.module.ts
@@ -1,23 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { InputTextModule } from 'primeng/inputtext';
-import { SidebarModule } from 'primeng/sidebar';
+import { RouterModule } from '@angular/router';
 import { BadgeModule } from 'primeng/badge';
-import { RadioButtonModule } from 'primeng/radiobutton';
 import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { RadioButtonModule } from 'primeng/radiobutton';
 import { RippleModule } from 'primeng/ripple';
+import { SidebarModule } from 'primeng/sidebar';
+
+import { AppFooterComponent } from './app.footer.component';
+import { AppLayoutComponent } from './app.layout.component';
 import { AppMenuComponent } from './app.menu.component';
 import { AppMenuitemComponent } from './app.menuitem.component';
-import { RouterModule } from '@angular/router';
+import { AppSidebarComponent } from './app.sidebar.component';
 import { AppTopBarComponent } from './app.topbar.component';
-import { AppFooterComponent } from './app.footer.component';
-import { AppConfigModule } from './config/config.module';
-import { AppSidebarComponent } from "./app.sidebar.component";
-import { AppLayoutComponent } from "./app.layout.component";
 
+//import { AppConfigModule } from './config/config.module';
 @NgModule({
     declarations: [
         AppMenuitemComponent,
@@ -39,7 +40,7 @@ import { AppLayoutComponent } from "./app.layout.component";
         InputSwitchModule,
         RippleModule,
         RouterModule,
-        AppConfigModule
+        //   AppConfigModule
     ],
     exports: [AppLayoutComponent]
 })
--- src/prime-ng.module.ts
+++ src/prime-ng.module.ts
@@ -1,32 +1,9 @@
 import { NgModule } from '@angular/core';
 import { ConfirmationService, MessageService } from 'primeng/api';
 import { ButtonModule } from 'primeng/button';
-import { CalendarModule } from 'primeng/calendar';
-import { CardModule } from 'primeng/card';
 import { ChartModule } from 'primeng/chart';
-import { CheckboxModule } from 'primeng/checkbox';
-import { ConfirmDialogModule } from 'primeng/confirmdialog';
-import { DataViewModule } from 'primeng/dataview';
-import { DialogModule } from 'primeng/dialog';
-import { DividerModule } from 'primeng/divider';
-import { DropdownModule } from 'primeng/dropdown';
-import { DynamicDialogModule } from 'primeng/dynamicdialog';
-import { FieldsetModule } from 'primeng/fieldset';
-import { InputNumberModule } from 'primeng/inputnumber';
 import { InputSwitchModule } from 'primeng/inputswitch';
-import { InputTextModule } from 'primeng/inputtext';
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { MenuModule } from 'primeng/menu';
-import { OverlayPanelModule } from 'primeng/overlaypanel';
-import { PanelModule } from 'primeng/panel';
-import { ProgressSpinnerModule } from 'primeng/progressspinner';
-import { SelectButtonModule } from 'primeng/selectbutton';
-import { StepsModule } from 'primeng/steps';
-import { StyleClassModule } from 'primeng/styleclass';
 import { TableModule } from 'primeng/table';
-import { TabViewModule } from 'primeng/tabview';
-import { TagModule } from 'primeng/tag';
-import { ToastModule } from 'primeng/toast';
 
 
 
@@ -36,33 +13,36 @@ export const primeNgServices = [
 ]
 
 const primeNgModules = [
-    ButtonModule,
-    DropdownModule,
-    ToastModule,
-    CheckboxModule,
-    CardModule,
-    DialogModule,
-    StepsModule,
-    InputTextModule,
-    DividerModule,
-    ProgressSpinnerModule,
-    DynamicDialogModule,
-    FieldsetModule,
-    CalendarModule,
-    InputTextareaModule,
     TableModule,
-    InputNumberModule,
-    ConfirmDialogModule,
-    TabViewModule,
-    MenuModule,
-    OverlayPanelModule,
-    DataViewModule,
     ChartModule,
-    TagModule,
-    StyleClassModule,
-    PanelModule,
-    SelectButtonModule,
-    InputSwitchModule
+    InputSwitchModule,
+    ButtonModule,
+
+    // DropdownModule,
+    // ToastModule,
+    // CheckboxModule,
+    // CardModule,
+    // DialogModule,
+    // StepsModule,
+    // InputTextModule,
+    // DividerModule,
+    // ProgressSpinnerModule,
+    // DynamicDialogModule,
+    // FieldsetModule,
+    // InputTextareaModule,
+
+    // InputNumberModule,
+    // ConfirmDialogModule,
+    // TabViewModule,
+    // MenuModule,
+    // OverlayPanelModule,
+    // DataViewModule,
+
+    // TagModule,
+    // StyleClassModule,
+    // PanelModule,
+    // SelectButtonModule,
+
 ];
 
 
